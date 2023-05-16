b34725146e626c415186e25f83f3ca93f5a713e4
Ruffaaw <rafalciupek07@gmail.com>
2023-05-16 22:21:53 +0200

    Create first spring boot application

---
+++ src/test/java/com/example/ztpai/ZtpaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ztpai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZtpaiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
