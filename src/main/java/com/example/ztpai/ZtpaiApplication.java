9e0c9b086e64cc4b53a76e1797988285b04fdb0a
Ruffaaw <rafalciupek07@gmail.com>
2023-06-05 23:43:24 +0200

    Improved main file

--- src/main/java/com/example/ztpai/ZtpaiApplication.java
+++ src/main/java/com/example/ztpai/ZtpaiApplication.java
@@ -1,13 +1,18 @@
 package com.example.ztpai;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @SpringBootApplication
 public class ZtpaiApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ZtpaiApplication.class, args);
     }
-
 }
