d9221fd72ef2e6ca2c92053414880bac96b17e67
Ruffaaw <rafalciupek07@gmail.com>
2023-06-05 23:39:43 +0200

    The product model was developed

--- src/main/java/com/example/ztpai/model/Product.java
+++ src/main/java/com/example/ztpai/model/Product.java
@@ -1,8 +1,12 @@
 package com.example.ztpai.model;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+
 public record Product(
         Integer id,
         Type type,
+        @NotBlank
         String name,
         Integer price,
         String image,
