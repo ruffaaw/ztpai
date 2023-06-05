f9fc57539945f59a703e00591d682e0fd53de0bc
Ruffaaw <rafalciupek07@gmail.com>
2023-06-05 23:41:10 +0200

    Created product controller

---
+++ src/main/java/com/example/ztpai/controllers/ProductController.java
@@ -0,0 +1,56 @@
+package com.example.ztpai.controllers;
+
+import com.example.ztpai.model.Product;
+import com.example.ztpai.repository.ProductCollectionRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/product")
+@CrossOrigin
+public class ProductController {
+
+    private final ProductCollectionRepository repository;
+
+    @Autowired
+    public ProductController(ProductCollectionRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping("")
+    public List<Product> findAll() {
+        return repository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Product findById(@PathVariable Integer id) {
+        return repository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Product not found"));
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("")
+    public void create(@Valid @RequestBody Product product) {
+        repository.save(product);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PutMapping("/{id}")
+    public void update(@RequestBody Product product, @PathVariable Integer id) {
+        if (!repository.existsById(id)) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Product not found");
+        }
+        repository.save(product);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Integer id) {
+        repository.delete(id);
+    }
+}
