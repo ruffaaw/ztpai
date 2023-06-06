61d4fe2087f6724c0aa8fdd3faf0eb3551f78ea3
Ruffaaw <rafalciupek07@gmail.com>
2023-06-06 19:41:39 +0200

    Added JPA annotations to ProductType

--- src/main/java/com/example/ztpai/model/ProductType.java
+++ src/main/java/com/example/ztpai/model/ProductType.java
@@ -1,8 +1,37 @@
 package com.example.ztpai.model;
 
-public enum ProductType {
-    DESKTOP,
-    LAPTOP,
-    SMARTPHONE,
-    TV
+import jakarta.persistence.*;
+
+@Entity(name = "products_type")
+public class ProductType {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "typename")
+    private String typename;
+
+    public ProductType() {
+    }
+
+    public ProductType(Integer id, String typename) {
+        this.id = id;
+        this.typename = typename;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTypename() {
+        return typename;
+    }
+
+    public void setTypename(String typename) {
+        this.typename = typename;
+    }
 }
