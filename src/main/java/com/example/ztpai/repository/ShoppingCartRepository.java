20c5e4661df2be576ed159658b1d9f74d2201da8
Ruffaaw <rafalciupek07@gmail.com>
2023-06-08 12:20:50 +0200

    Changed ShoppingCartRepository from class to interface extend JpaRepository

--- src/main/java/com/example/ztpai/repository/ShoppingCartRepository.java
+++ src/main/java/com/example/ztpai/repository/ShoppingCartRepository.java
@@ -1,6 +1,7 @@
 package com.example.ztpai.repository;
 
 import com.example.ztpai.model.ShoppingCart;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
@@ -9,29 +10,5 @@ import java.util.Optional;
 import java.util.UUID;
 
 @Repository
-public class ShoppingCartRepository {
-    public ShoppingCartRepository() {
-
-    }
-
-    private final List<ShoppingCart> shoppingCarts = new ArrayList<>();
-
-    public List<ShoppingCart> findAll() {
-        return shoppingCarts;
-    }
-
-    public Optional<ShoppingCart> findById(UUID id) {
-        return shoppingCarts.stream()
-                .filter(cart -> cart.getId().equals(id))
-                .findFirst();
-    }
-
-    public void save(ShoppingCart shoppingCart) {
-        shoppingCarts.add(shoppingCart);
-    }
-
-    public void deleteById(UUID id) {
-        shoppingCarts.removeIf(cart -> cart.getId().equals(id));
-    }
-
+public interface ShoppingCartRepository extends JpaRepository<ShoppingCart, UUID> {
 }
