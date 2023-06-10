c42e444f3ed547ef05bc28ba35b8c6e15ebb04b5
Ruffaaw <rafalciupek07@gmail.com>
2023-06-10 12:15:15 +0200

    Added page displaying computers

---
+++ frontend/src/routes/products-desktops-page.js
@@ -0,0 +1,49 @@
+/** @format */
+
+import React, { useState, useEffect } from "react";
+import HeaderBar from "../components/header";
+import Categories from "../components/categories";
+import axios from "axios";
+import "../css/products.css";
+
+function ProductsDesktopsPage() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:8080/api/products/type/2")
+      .then((response) => {
+        setProducts(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching products:", error);
+      });
+  }, []);
+  return (
+    <div className="productsMainContainer">
+      <div className="headerContainer">
+        <HeaderBar />
+      </div>
+      <div className="mainContainer">
+        <div className="mainCategoriesContainer">
+          <Categories />
+        </div>
+        <div className="productsContainer">
+          {products.map((product) => (
+            <div key={product.id} className="productItem">
+              {/* <img src={require(product.image)} alt={product.name} /> */}
+              {console.log(product.image)}
+              <h3>{product.name}</h3>
+              <p>{product.price} PLN</p>
+              <div className="addToCartButton">
+                <button>Add to cart</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ProductsDesktopsPage;
