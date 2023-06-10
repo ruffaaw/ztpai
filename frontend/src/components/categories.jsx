fa3373ce22792cef7333dd856324685ae1c03728
Ruffaaw <rafalciupek07@gmail.com>
2023-06-10 12:13:03 +0200

    Added categories component

--- frontend/src/components/categories.jsx
+++ frontend/src/components/categories.jsx
@@ -1,6 +1,6 @@
 /** @format */
 
-import React from "react";
+import React, { useState } from "react";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
@@ -10,8 +10,17 @@ import {
   faMobile,
   faTv,
 } from "@fortawesome/free-solid-svg-icons";
-// import "../css/components.css";
+import { useNavigate } from "react-router-dom";
+
 function Categories() {
+  const [selectedCategory, setSelectedCategory] = useState(null);
+  const navigate = useNavigate();
+
+  const handleCategoryClick = (category) => {
+    setSelectedCategory(category);
+    navigate(`/products/${selectedCategory}`);
+  };
+
   return (
     <div className="categoriesContainer">
       <div classname="categories">
@@ -20,25 +29,37 @@ function Categories() {
       </div>
       <div classname="categoriesProducts">
         <FontAwesomeIcon className="categoriesProductsIcon" icon={faDesktop} />
-        <button className="categoriesButton">
+        <button
+          className="categoriesButton"
+          onClick={() => handleCategoryClick("desktop")}
+        >
           <label className="categoriesProductsLabel">Desktop</label>
         </button>
       </div>
       <div classname="categoriesProducts">
         <FontAwesomeIcon className="categoriesProductsIcon" icon={faLaptop} />
-        <button className="categoriesButton">
+        <button
+          className="categoriesButton"
+          onClick={() => handleCategoryClick("laptop")}
+        >
           <label className="categoriesProductsLabel">Laptop</label>
         </button>
       </div>
       <div classname="categoriesProducts">
         <FontAwesomeIcon className="categoriesProductsIcon" icon={faMobile} />
-        <button className="categoriesButton">
+        <button
+          className="categoriesButton"
+          onClick={() => handleCategoryClick("smartphone")}
+        >
           <label className="categoriesProductsLabel">Smartphone</label>
         </button>
       </div>
       <div classname="categoriesProducts">
         <FontAwesomeIcon className="categoriesProductsIcon" icon={faTv} />
-        <button className="categoriesButton">
+        <button
+          className="categoriesButton"
+          onClick={() => handleCategoryClick("tv")}
+        >
           <label className="categoriesProductsLabel">TV</label>
         </button>
       </div>
