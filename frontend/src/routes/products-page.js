7d7519d99d1342f8f6b88f930e69ec1d4ccd1234
Ruffaaw <rafalciupek07@gmail.com>
2023-06-10 12:17:08 +0200

    Added page displaying prodycts integral with REST API

---
+++ frontend/src/routes/products-page.js
@@ -0,0 +1,72 @@
+/** @format */
+
+import React, { useState, useEffect } from "react";
+import HeaderBar from "../components/header";
+import Categories from "../components/categories";
+import axios from "axios";
+import "../css/products.css";
+
+function ProductsPage() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:8080/api/products")
+      .then((response) => {
+        setProducts(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching products:", error);
+      });
+  }, []);
+
+  const addToCart = (cartItem) => {
+    console.log(cartItem);
+    const cartItemRequest = {
+      products: {
+        id: cartItem.id,
+      },
+      quantity: 1,
+    };
+    axios
+      .post(
+        `http://localhost:8080/api/shopping-cart/a53ce94c-bf0d-4f64-868f-6beccd9e3eea/add-item`,
+        cartItemRequest
+      )
+      .then((response) => {
+        // Przetwórz odpowiedź po dodaniu przedmiotu do koszyka
+        console.log("Item added to cart:", response.data);
+      })
+      .catch((error) => {
+        console.error("Error adding item to cart:", error);
+      });
+  };
+
+  return (
+    <div className="productsMainContainer">
+      <div className="headerContainer">
+        <HeaderBar />
+      </div>
+      <div className="mainContainer">
+        <div className="mainCategoriesContainer">
+          <Categories />
+        </div>
+        <div className="productsContainer">
+          {products.map((product) => (
+            <div key={product.id} className="productItem">
+              {/* <img src={require(product.image)} alt={product.name} /> */}
+              {console.log(product.image)}
+              <h3>{product.name}</h3>
+              <p>{product.price} PLN</p>
+              <div className="addToCartButton">
+                <button onClick={() => addToCart(product)}>Add to cart</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ProductsPage;
