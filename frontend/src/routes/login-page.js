d163d4da5d9159fdb7319b236f311bc12121e3b1
Ruffaaw <rafalciupek07@gmail.com>
2023-06-12 14:19:39 +0200

    Added login system

--- frontend/src/routes/login-page.js
+++ frontend/src/routes/login-page.js
@@ -3,6 +3,7 @@
 import React, { useState } from "react";
 import logo from "../img/logos/logo.svg";
 import "../css/login.css";
+import axios from "axios";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faEyeSlash,
@@ -10,14 +11,55 @@ import {
   faEnvelope,
   faLock,
 } from "@fortawesome/free-solid-svg-icons";
+import { useNavigate } from "react-router-dom";
 
 function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [showPassword, setShowPassword] = useState(false);
+
+  const navigate = useNavigate();
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSignUpClick = () => {
+    navigate(`/signup`);
+  };
 
   const handleShowPassword = (e) => {
     console.log(showPassword);
     e.preventDefault();
     setShowPassword(!showPassword);
+  };
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post(
+        "http://localhost:8080/api/person/login",
+        {
+          email,
+          password,
+        }
+      );
+      console.log(response.data);
+
+      if (response.status === 200) {
+        window.location.href = "/products";
+      } else {
+        alert("Wrong email or password, please try again");
+      }
+    } catch (error) {
+      console.log(error);
+      alert("Wrong email or password, please try again");
+    }
   };
 
   return (
@@ -35,6 +77,8 @@ function LoginPage() {
               className="email"
               type="text"
               placeholder="email@email.com"
+              value={email}
+              onChange={handleEmailChange}
             />
           </div>
           <div className="inputDatePassword">
@@ -45,6 +89,8 @@ function LoginPage() {
               className="password"
               type={showPassword ? "text" : "password"}
               placeholder="password"
+              value={password}
+              onChange={handlePasswordChange}
             />
             <button className="eyeButton" onClick={handleShowPassword}>
               {showPassword ? (
@@ -55,10 +101,14 @@ function LoginPage() {
             </button>
           </div>
           <div className="buttons">
-            <button className="loginButton" type="submit">
+            <button className="loginButton" type="submit" onClick={handleLogin}>
               LOGIN
             </button>
-            <button className="signUpButton" type="submit">
+            <button
+              className="signUpButton"
+              type="submit"
+              onClick={handleSignUpClick}
+            >
               SIGN UP
             </button>
           </div>
