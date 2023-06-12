e332b1fe73b95354f6878375c99fc47dbe24bd08
Ruffaaw <rafalciupek07@gmail.com>
2023-06-12 14:19:11 +0200

    Corrected index and App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,33 @@
 /** @format */
+import { Routes, Route } from "react-router-dom";
+import LoginPage from "./routes/login-page";
+import SignUpPage from "./routes/signup-page";
+import ProductsPage from "./routes/products-page";
+import ProductsDesktopsPage from "./routes/products-desktops-page";
+import ProductsLaptopsPage from "./routes/products-laptops-page";
+import ProductsSmartphonesPage from "./routes/products-smartphones-page";
+import ProductsTvPage from "./routes/products-tv-page";
+import ShoppingCart from "./routes/shopping-cart";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<LoginPage />}></Route>
+      <Route path="/login" element={<LoginPage />}></Route>
+      <Route path="/signup" element={<SignUpPage />}></Route>
+      <Route path="/products" element={<ProductsPage />}></Route>
+      <Route
+        path="/products/desktop"
+        element={<ProductsDesktopsPage />}
+      ></Route>
+      <Route path="/products/laptop" element={<ProductsLaptopsPage />}></Route>
+      <Route
+        path="/products/smartphone"
+        element={<ProductsSmartphonesPage />}
+      ></Route>
+      <Route path="/products/tv" element={<ProductsTvPage />}></Route>
+      <Route path="/shoppingcart" element={<ShoppingCart />}></Route>
+    </Routes>
   );
 }
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,34 +3,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import LoginPage from "./routes/login-page";
-import SignUpPage from "./routes/signup-page";
-import ProductsPage from "./routes/products-page";
-import ProductsDesktopsPage from "./routes/products-desktops-page";
-import ProductsLaptopsPage from "./routes/products-laptops-page";
-import ProductsSmartphonesPage from "./routes/products-smartphones-page";
-import ProductsTvPage from "./routes/products-tv-page";
-import ShoppingCart from "./routes/shopping-cart";
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<LoginPage />}></Route>
-      <Route path="/login" element={<LoginPage />}></Route>
-      <Route path="/signup" element={<SignUpPage />}></Route>
-      <Route path="/products" element={<ProductsPage />}></Route>
-      <Route
-        path="/products/desktop"
-        element={<ProductsDesktopsPage />}
-      ></Route>
-      <Route path="/products/laptop" element={<ProductsLaptopsPage />}></Route>
-      <Route
-        path="/products/smartphone"
-        element={<ProductsSmartphonesPage />}
-      ></Route>
-      <Route path="/products/tv" element={<ProductsTvPage />}></Route>
-      <Route path="/shoppingcart" element={<ShoppingCart />}></Route>
-    </Routes>
+    <App />
   </BrowserRouter>
 );
